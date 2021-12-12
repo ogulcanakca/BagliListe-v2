06a33b7f243f7bc29918bb047d9a827f378c2ec9
ogulcanakca <68269829+ogulcanakca@users.noreply.github.com>
2021-12-12 17:18:43 +0300

    +

--- BagliListe v2/BagliListe.hpp
+++ BagliListe v2/BagliListe.hpp
@@ -34,7 +34,17 @@ public:
 			gec->sonraki = yeni;
 		}
 	}
+	Dugum<Tur>* sondanBirOncekiniGetir() {
+
+		Dugum<Tur>* gec = ilk;
+
+		while (gec->sonraki->sonraki != 0) {
+			gec = gec->sonraki;
+		}
+		return gec;
+	}
 	void yazdir() {
+		cout << endl;
 		cout << setw(10) << "adres" << setw(10) << "veri" << setw(15) << "sonraki" << endl;
 		cout << "-------------------------------------------------------------------" << endl;
 		Dugum<Tur>* gec = ilk;
@@ -44,6 +54,25 @@ public:
 			gec = gec->sonraki;
 		}
 		cout << endl;
+	}
+
+	void cikar() {
+		if (ilk == 0) {
+			return;
+		}
+		if (ilk->sonraki == 0) {
+			delete ilk;
+			ilk = 0;
+
+		}
+		else {
+			Dugum<Tur>* gec = sondanBirOncekiniGetir();
+
+				cout << gec << ". cikarildi" << endl;
+				delete gec->sonraki;
+				gec->sonraki = 0;
+			
+		}
 	}
 private:
 	Dugum<Tur>* ilk;
--- BagliListe v2/main.cpp
+++ BagliListe v2/main.cpp
@@ -5,19 +5,20 @@ using namespace std;
 int main()
 {
 	BagliListe<int>* b = new BagliListe<int>();
-
 	b->ekle(1);
 	b->ekle(2);
 	b->ekle(3);
 	b->ekle(4);
+	
+	b->yazdir();
+	b->cikar();
 	b->ekle(5);
 	b->ekle(6);
 	b->ekle(7);
 	b->ekle(8);
 	b->ekle(9);
-
 	b->yazdir();
 	delete b;
-
+	
 }
 
